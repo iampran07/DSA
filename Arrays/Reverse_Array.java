d3ee09a67b3f65f9d164682786ef797867a64c5b
Pranav Prakash Patki <91589434+iampran07@users.noreply.github.com>
2023-08-23 18:08:40 +0530

    Update Reverse_Array.java

--- Arrays/Reverse_Array.java
+++ Arrays/Reverse_Array.java
@@ -1,1 +1,37 @@
+//Reverse of an array without using in-built function
+//Write a swap of an array in Java
+//Reverse of an array in java
 
+
+import java.util.Scanner;
+
+class Main
+{
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int arr[]=new int[n];
+
+        for(int i=0;i<n;i++)
+        {
+            arr[i]=sc.nextInt();
+        }
+
+        int i=0;
+        int j=n-1;
+
+        while(i<j){
+            int temp=arr[i];
+            arr[i]=arr[j];
+            arr[j]=temp;
+            i++;
+            j--;
+        }
+    System.out.println("Reverse of an array is: ");
+        for(int k=0;k<n;k++)
+        {
+            System.out.println(arr[k]);
+        }
+    }
+}
